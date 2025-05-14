6d146c6307458fd80f94db70bd516f3c05a54ab1
Edgar Cancino <edgarcancinoe@gmail.com>
2025-05-14 13:10:51 +0200

    Missing arduino.h

--- iot_assignment/config.h
+++ iot_assignment/config.h
@@ -36,18 +36,18 @@
 #define MQTT_TASK_RATE 10 // MS
 
 // WiFi Configuration
-// const char* ssid = "FRITZ!Box 7530 LP";
-// const char* password = "70403295595551907386";
+const char* ssid = "FRITZ!Box 7530 LP";
+const char* password = "70403295595551907386";
 
-const char* ssid = "iPhone de Edgar";
-const char* password = "01234567";
+// const char* ssid = "iPhone de Edgar";
+// const char* password = "01234567";
 
 // const char* ssid = "PortofinoHomes";
 // const char* password = "Portofinohomes_2203";
 
 // MQTT Configuration
-// const char* mqtt_server = "192.168.178.50"; // Rome
-const char* mqtt_server    = "172.20.10.2"; // Hotspot
+const char* mqtt_server = "192.168.178.50"; // Rome
+// const char* mqtt_server    = "172.20.10.2"; // Hotspot
 // const char* mqtt_server = "192.168.0.108"; // Altro
 
 const int mqtt_port = 1883;
--- iot_assignment/iot_assignment.ino
+++ iot_assignment/iot_assignment.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <SPI.h> 
 #include "config.h"
 #include "HT_SSD1306Wire.h"
